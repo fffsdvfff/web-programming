2c7733a3171477c77525aff8d318bae547dd7f99
Ella Sivaeva <145915722+fffsdvfff@users.noreply.github.com>
2026-04-25 14:58:33 +0300

    car

--- lab2/script.js
+++ lab2/script.js
@@ -1,2 +1,20 @@
 console.log("Lab 2 works");
 
+// 1. car1 через new Object()
+
+let car1 = new Object();
+
+car1.color = "red";
+car1.maxSpeed = 180;
+
+car1.driver = {
+    name: "Елла Сіваєва",
+    category: "C",
+    personalLimitations: "No driving at night"
+};
+
+car1.tuning = true;
+car1.numberOfAccidents = 0;
+
+// перевірка
+console.log(car1);
