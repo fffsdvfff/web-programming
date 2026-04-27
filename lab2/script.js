f19ef8fc5105808dd6d1931daa7953b15b06d2f4
Ella Sivaeva <145915722+fffsdvfff@users.noreply.github.com>
2026-04-27 16:48:37 +0300

    ivan

--- lab2/script.js
+++ lab2/script.js
@@ -45,3 +45,49 @@ car2.drive = function () {
 };
 
 car2.drive();
+
+function Truck(color, weight, avgSpeed, brand, model) {
+    this.color = color;
+    this.weight = weight;
+    this.avgSpeed = avgSpeed;
+    this.brand = brand;
+    this.model = model;
+
+    // метод trip
+    this.trip = function () {
+        if (!this.driver) {
+            console.log("No driver assigned");
+        } else {
+            let message = "Driver " + this.driver.name + " ";
+
+            if (this.driver.nightDriving) {
+                message += "drives at night ";
+            } else {
+                message += "does not drive at night ";
+            }
+
+            message += "and has " + this.driver.experience + " years of experience";
+
+            console.log(message);
+        }
+    };
+}
+
+Truck.prototype.AssignDriver = function (name, nightDriving, experience) {
+    this.driver = {
+        name: name,
+        nightDriving: nightDriving,
+        experience: experience
+    };
+};
+
+let truck1 = new Truck("white", 5000, 90, "Volvo", "FH16");
+let truck2 = new Truck("black", 4500, 80, "MAN", "TGX");
+
+// додаємо водіїв
+truck1.AssignDriver("Ivan Ivanov", true, 5);
+truck2.AssignDriver("Petro Petrov", false, 3);
+
+// перевірка
+truck1.trip();
+truck2.trip();
