181c5d4250d56c79fc4b037a291c65bee5819d1d
Ella Sivaeva <145915722+fffsdvfff@users.noreply.github.com>
2026-04-28 15:46:05 +0300

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -1,1 +1,25 @@
+(function () {
+    var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+        if (firstLetter === "j") {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+
+    console.log("Додатковий спосіб відбору: якщо ім'я закінчується на літеру 'a', виводиться Good Bye, інакше Hello.");
+
+    for (var i = 0; i < names.length; i++) {
+        var lastLetter = names[i].charAt(names[i].length - 1).toLowerCase();
+
+        if (lastLetter === "a") {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
