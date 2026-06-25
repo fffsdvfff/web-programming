a57d9e10a30a94585cb74edd227f4c380125bb23
Ella Sivaeva <145915722+fffsdvfff@users.noreply.github.com>
2026-06-25 21:01:16 +0300

    додавання прод джейсон

--- team-project/script.js
+++ team-project/script.js
@@ -28,3 +28,22 @@ helloBtn.addEventListener("click", function() {
 
   userText.style.background = "white";
 });
+
+let productsList = document.getElementById("productsList");
+
+fetch("data/products.json")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(products) {
+    products.forEach(function(item) {
+      productsList.innerHTML += `
+        <div class="product-card">
+          <div class="product-img">${item.icon}</div>
+          <h3>${item.name}</h3>
+          <p class="category">${item.category}</p>
+          <p>${item.description}</p>
+        </div>
+      `;
+    });
+  });
